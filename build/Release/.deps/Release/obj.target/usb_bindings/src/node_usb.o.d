30d53980c4a834c20babe077597fcb475f0de983
MandeepChaudhary <38320473+MandeepChaudhary@users.noreply.github.com>
2024-10-17 09:43:02 +0200

    commit usb for mac npm module

---
+++ build/Release/.deps/Release/obj.target/usb_bindings/src/node_usb.o.d
@@ -0,0 +1,27 @@
+cmd_Release/obj.target/usb_bindings/src/node_usb.o := c++ -o Release/obj.target/usb_bindings/src/node_usb.o ../src/node_usb.cc '-DNODE_GYP_MODULE_NAME=usb_bindings' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-DELECTRON_ENSURE_CONFIG_GYPI' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DUSING_ELECTRON_CONFIG_GYPI' '-DV8_COMPRESS_POINTERS' '-DV8_COMPRESS_POINTERS_IN_SHARED_CAGE' '-DV8_ENABLE_SANDBOX' '-DV8_31BIT_SMIS_ON_64BIT_ARCH' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DOPENSSL_NO_ASM' '-DNAPI_VERSION=8' '-DBUILDING_NODE_EXTENSION' -I../src -I/Users/arpitnagpal/Workspace/MandeepChaudhary/pos/node_modules/usb/node_modules/node-addon-api -I/Users/arpitnagpal/.electron-gyp/29.4.5/include/node -I/Users/arpitnagpal/.electron-gyp/29.4.5/src -I/Users/arpitnagpal/.electron-gyp/29.4.5/deps/openssl/config -I/Users/arpitnagpal/.electron-gyp/29.4.5/deps/openssl/openssl/include -I/Users/arpitnagpal/.electron-gyp/29.4.5/deps/uv/include -I/Users/arpitnagpal/.electron-gyp/29.4.5/deps/zlib -I/Users/arpitnagpal/.electron-gyp/29.4.5/deps/v8/include -I../libusb/libusb  -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.0.sdk -O3 -gdwarf-2 -mmacosx-version-min=10.7 -arch arm64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++17 -stdlib=libc++ -fno-rtti -fno-strict-aliasing -std=c++1y -stdlib=libc++ -arch x86_64 -arch arm64 -MMD -MF ./Release/.deps/Release/obj.target/usb_bindings/src/node_usb.o.d.raw   -c
+Release/obj.target/usb_bindings/src/node_usb.o: ../src/node_usb.cc \
+  ../src/node_usb.h ../libusb/libusb/libusb.h \
+  /Users/arpitnagpal/Workspace/MandeepChaudhary/pos/node_modules/usb/node_modules/node-addon-api/napi.h \
+  /Users/arpitnagpal/.electron-gyp/29.4.5/include/node/node_api.h \
+  /Users/arpitnagpal/.electron-gyp/29.4.5/include/node/js_native_api.h \
+  /Users/arpitnagpal/.electron-gyp/29.4.5/include/node/js_native_api_types.h \
+  /Users/arpitnagpal/.electron-gyp/29.4.5/include/node/node_api_types.h \
+  /Users/arpitnagpal/Workspace/MandeepChaudhary/pos/node_modules/usb/node_modules/node-addon-api/napi-inl.h \
+  /Users/arpitnagpal/Workspace/MandeepChaudhary/pos/node_modules/usb/node_modules/node-addon-api/napi-inl.deprecated.h \
+  ../src/helpers.h ../src/uv_async_queue.h ../src/thread_name.h \
+  ../src/hotplug/hotplug.h ../src/hotplug/../node_usb.h
+../src/node_usb.cc:
+../src/node_usb.h:
+../libusb/libusb/libusb.h:
+/Users/arpitnagpal/Workspace/MandeepChaudhary/pos/node_modules/usb/node_modules/node-addon-api/napi.h:
+/Users/arpitnagpal/.electron-gyp/29.4.5/include/node/node_api.h:
+/Users/arpitnagpal/.electron-gyp/29.4.5/include/node/js_native_api.h:
+/Users/arpitnagpal/.electron-gyp/29.4.5/include/node/js_native_api_types.h:
+/Users/arpitnagpal/.electron-gyp/29.4.5/include/node/node_api_types.h:
+/Users/arpitnagpal/Workspace/MandeepChaudhary/pos/node_modules/usb/node_modules/node-addon-api/napi-inl.h:
+/Users/arpitnagpal/Workspace/MandeepChaudhary/pos/node_modules/usb/node_modules/node-addon-api/napi-inl.deprecated.h:
+../src/helpers.h:
+../src/uv_async_queue.h:
+../src/thread_name.h:
+../src/hotplug/hotplug.h:
+../src/hotplug/../node_usb.h:
